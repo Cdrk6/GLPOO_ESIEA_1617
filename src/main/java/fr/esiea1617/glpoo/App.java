66ed607fcc82daed4f56802a20c8ab2afd91638a
Stalker <arnaud.vadez@gmail.com>
2017-03-27 14:55:41 +0200

    Arnaud test succed

--- src/main/java/fr/esiea1617/glpoo/App.java
+++ src/main/java/fr/esiea1617/glpoo/App.java
@@ -1,13 +1,21 @@
 package fr.esiea1617.glpoo;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+
+	
+    public static void main( String[] args ) {
+    	
+    	BasicConfigurator.configure();
+        //System.out.println( "Hello World!" );
+    	LOGGER.debug("Hello world!");
     }
 }
