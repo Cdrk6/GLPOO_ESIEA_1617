0292b3b3cdf025c0d5adb6d3511e1c8c12f16596
Jean-Yann <lyjeanyann@outlook.com>
2017-04-26 15:59:49 +0200

    affichage de l'image d'acceuil

--- src/main/java/fr/esiea1617/glpoo/ihm/ProjetJFrame.java
+++ src/main/java/fr/esiea1617/glpoo/ihm/ProjetJFrame.java
@@ -1,7 +1,12 @@
 package fr.esiea1617.glpoo.ihm;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 //import javax.swing.JScrollPane;
 //import javax.swing.JTable;
 
@@ -11,9 +16,17 @@ public class ProjetJFrame extends JFrame{
 	
 	public ProjetJFrame() {
         setTitle("Chasse aux oeufs");
-        setPreferredSize(new Dimension(500, 400));
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //setPreferredSize(new Dimension(700, 760));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        ImageIcon image = new ImageIcon("src/main/ressources/menu.png");
+        JLabel label = new JLabel(image);
+        JScrollPane scrollPane = new JScrollPane(label);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        add(scrollPane, BorderLayout.CENTER);
         pack();
+        
     }
 	
 }
