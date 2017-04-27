f12373f086dcd2d2f104fc7b2ccf413a619e0cd7
vbonneau <victor.bonneau27@gmail.com>
2017-04-27 16:22:17 +0200

    test child go in wall

--- src/test/java/fr/esiea1617/glpoo/domain/ChildTest.java
+++ src/test/java/fr/esiea1617/glpoo/domain/ChildTest.java
@@ -12,22 +12,42 @@ import org.junit.Test;
 public class ChildTest {
 	
 	private Child child;
+	private Child childGoWall;
 	
 	@Before 
 	 public void doBefore(){
+		 Map m= new Map();
 		 ArrayList<Character> displ=new ArrayList<Character>();
 		 displ.add('A');
-		 
-		 Map m= new Map();
-		 
 		 child = new Child(0,0,'E',displ,"buu",m);
+		 childGoWall = new Child(5,0,'N',displ,"Wall",m);
+		 
 	 }
 	
 	 @Test
-		public void testmove(){
+		public void testmoveX(){
 		 child.move();
 		 assertEquals(1,child.getX());
 		 
+	 }
+	 
+	 @Test
+		public void testmoveY(){
+		 child.move();
+		 assertEquals(0,child.getY());
+		 
+	 }
+	 
+	 @Test
+	 public void testInWallX(){
+		 childGoWall.move();
+		 assertEquals(5,childGoWall.getX());
+	 }
+	 
+	 @Test
+	 public void testInWallY(){
+		 childGoWall.move();
+		 assertEquals(0,childGoWall.getY());
 	 }
 		
 		
